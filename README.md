1927c8d16a2cb8fab9e015b88d385a42a6f0e5e4
Parth1701 <72870837+Parth1701@users.noreply.github.com>
2021-02-12 15:02:26 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# monkey
+# AngryBirdsStage7
--- sketch.js
+++ sketch.js
@@ -1,129 +1,102 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
-var monkey , monkey_running
-var banana ,bananaImage, obstacle, obstacleImage
-var FoodGroup, obstacleGroup
-var score
+var engine, world;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
 
-function preload(){
-  
-  
-  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
-  
-  bananaImage = loadImage("banana.png");
-  obstaceImage = loadImage("obstacle.png");
- 
+var gameState = "onSling";
+var bg = "sprites/bg1.png";
+var score = 0;
+
+function preload() {
+   backgroundImg = loadImage("sprites/bg.png");
 }
 
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
 
 
-function setup() {
-  // createCanvas(600, 600);
-  
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
 
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
 
-  var survivalTime=0;
-  
-  //creating monkey
-   monkey=createSprite(80,315,20,20);
-   monkey.addAnimation("moving", monkey_running);
-  // monkey.addImage(bananaImage)
-   monkey.scale=0.1
-  
-  ground = createSprite(400,350,900,10);
-  ground.velocityX=-4;
-  ground.x=ground.width/2;
-  console.log(ground.x)
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
 
-  FoodGroup = new Group();
-  obstaclesGroup = new Group();
+    log3 =  new Log(810,180,300, PI/2);
 
-  score = 0;
- 
-  
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(200,50);
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
+function draw(){
+  
+        background(backgroundImg);
+    
+        noStroke();
+        textSize(35)
+        fill("white")
+        text("Score  " + score, width-300, 50)
+    
+    Engine.update(engine);
+    //strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    pig1.score();
+    log1.display();
 
-function draw() {
-  
-  background(255);
-  monkey.vilocityY=0.9;     
-    
-  if(ground.x<0) {
-    ground.x=ground.width/2;
-  }
-  
-  console.log(monkey.y)
-   
-    if(keyDown("space") && (monkey.y>314) ) {
-      monkey.velocityY = -15;
-    }
-    monkey.velocityY = monkey.velocityY + 0.8;
-  
-    monkey.collide(ground);   
-    spawnFood();
-    spawnObstacles();
- 
-  drawSprites();
-  stroke("white");
-  textSize(20);
-  fill("white");
-  text("Score: "+ score, 500,50);        
-  
-  
-    
-  
-  stroke("black");
-  textSize(20);
-  fill("black");
-  survivalTime=Math.round(frameCount/frameRate()) 
-  text("Survival Time: "+ survivalTime, 100,50);
-  if(obstaclesGroup.isTouching(monkey)){
-        ground.velocityX = 0;
-        monkey.velocityY = 0;
-        obstaclesGroup.setVelocityXEach(0);
-        FoodGroup.setVelocityXEach(0);
-        obstaclesGroup.setLifetimeEach(-1);
-        FoodGroup.setLifetimeEach(-1);
-    survivalTime=0;
-    
+    box3.display();
+    box4.display();
+    pig3.display();
+    pig3.score();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+    platform.display();
+    //log6.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
     }
 }
 
 
-
-function spawnFood() {
-  //write code here to spawn the Food
-  if (frameCount % 80 === 0) {
-    banana = createSprite(600,250,40,10);
-    banana.y = random(120,200);    
-    banana.velocityX = -5;
-    
-     //assign lifetime to the variable
-    banana.lifetime = 300;
-    monkey.depth = banana.depth + 1;
-    
-    //add image of banana
-     banana.addImage(bananaImage);
-     banana.scale=0.05;
-    
-    //add each banana to the group
-    FoodGroup.add(banana);
-  }
+function mouseReleased(){
+    slingshot.fly();
+    gameState = "launched";
 }
 
-function spawnObstacles() {
-  if(frameCount % 300 === 0) {
-    obstacle = createSprite(800,320,10,40);
-    obstacle.velocityX = -6;
-    
-    //add image to the obstacle 
-    obstacle.addImage(obstaceImage);
-    obstacle.scale=0.15;
-    
-    //lifetime to the obstacle     
-    obstacle.lifetime = 300;
-    
-    //add each obstacle to the group
-    obstaclesGroup.add(obstacle);
-  }
+function keyPressed(){
+    if(keyCode === 32){
+    gameState="onSling";
+     bird.trajectory = [];
+     Matter.Body.setPosition(bird.body,{x:200, y:50});
+       slingshot.attach(bird.body);
+    }
 }
